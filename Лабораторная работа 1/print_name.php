1b882daf4abd586f7d324e4d3679056e4cb154c7
LAssiphone <VLK@digt.ru>
2015-04-18 14:47:01 +0300

    1

---
+++ Лабораторная работа 1/print_name.php
@@ -0,0 +1,3 @@
+<?php
+	header('Content-Type: text/plain');
+	echo "Hello, Dear ".$_GET["name"]."!";
---
+++ Лабораторная работа 1/print_query_string.php
@@ -0,0 +1,3 @@
+<?php
+	header('Content-Type: text/plain');
+	echo "Query string = '".$_SERVER["QUERY_STRING"]."'";
