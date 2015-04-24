027ec2b91e764415b93c4654174ff764a0f4bced
LAssiphone <VLK@digt.ru>
2015-04-24 21:47:05 +0300

    Update calc_sum.php

--- Лабораторная работа 1/calc_sum.php
+++ Лабораторная работа 1/calc_sum.php
@@ -1,5 +1,5 @@
 <?php
-    if (isset ($_GET["arg1"]) ? $_GET["arg1"] : '');
-    if (isset ($_GET["arg2"]) ? $_GET["arg2"] : '');
-    $sum = $_GET["arg1"] + $_GET["arg2"];
+    $arg1 = isset ($_GET["arg1"]) ? $_GET["arg1"] : 0;
+    $arg2 = isset ($_GET["arg2"]) ? $_GET["arg2"] : 0;
+    $sum = $arg1 + $arg2;
     echo $sum;
