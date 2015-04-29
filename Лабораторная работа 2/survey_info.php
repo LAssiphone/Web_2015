7f0bb4208966e78fe5f073f2b83c9226eb42d5a6
LAssiphone <VLK@digt.ru>
2015-04-29 11:14:42 +0300

    Update survey_info.php

--- Лабораторная работа 2/survey_info.php
+++ Лабораторная работа 2/survey_info.php
@@ -1,16 +1,16 @@
 <?php
     include 'include/common.inc.php';
-    // Проверяем наличие вводимых данных
+    
     $email = GetParamFromGet('email');
-    // Создаем переменную пути к файлу
+    
     $file = "data/{$email}.txt";
-    // Проверка - существует ли файл    
+      
     if (!file_exists($file))
     {
         echo 'E-mail can not be found , please try again';
         return;
     }
-    // выводим файл, с проверкой его указания
+    
     if (isset($_GET['email']))
     {
         $data = file($file);
